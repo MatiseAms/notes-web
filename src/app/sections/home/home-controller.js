ce16c34e953d97e8ee86aa76df90bb161bf72346
Maurits Meester <maurits@matise.nl>
2016-09-13 21:30:01 +0200

    commercial detection

--- src/app/sections/home/home-controller.js
+++ src/app/sections/home/home-controller.js
@@ -6,14 +6,25 @@ angular.module('notesweb')
 		self.hello = 'hello';
 
 		angular.extend($scope,{
-			nowPlaying: {}
+			nowPlaying: {},
+			notifications: []
 		});
 
 		ProgrammingService.now().then(function(data){
-			$scope.nowPlaying = data;
+			if(data!==false){
+				$scope.notifications.push({type: 'notification','text':'Oh no commercials 😫, need a website? Call +31 (0) 20 845 3799!'});
+			}else{
+				$scope.nowPlaying = data;
+			}
 		},function(error){
 
 		});
+
+		setTimeout(function(){
+			$scope.notifications.push({type: 'notification','text':'Oh no commercials 😫, need a website? Call +31 (0) 20 845 3799!'});
+			$scope.$applyAsync();
+			// console.log($scope.notifications);
+		},1000);
 
 		$scope.spinner = false;
     setTimeout(function(){
