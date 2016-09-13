84b9aaf547b86965d1986a3956087fd24873ec87
Maurits Meester <maurits@matise.nl>
2016-09-13 21:03:08 +0200

    hoppa from api

---
+++ src/app/components/services/programming-service.js
@@ -0,0 +1,20 @@
+angular.module('notesweb')
+	.service('ProgrammingService', ['$q','$http', function($q,$http) {
+		'use strict';
+
+		var self = this;
+
+		self.now = function now() {
+      var deferObj = $q.defer();
+      
+      $http.get('http://api.notes.matise.nl/programming/now').then(function(data){
+        console.log(data);
+        deferObj.resolve(data);
+      },function(error){
+        deferObj.reject(error);
+      });
+
+      return deferObj.promise;
+		};
+
+	}]);
